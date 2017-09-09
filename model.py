43f0de50ba5b8116dea44f4cd5ca8241061a1641
Dileep Bapat <dileepbapat@gmail.com>
2017-09-10 03:27:44 +0530

    Dileep | removing unused code

--- model.py
+++ model.py
@@ -37,42 +37,6 @@ def file_path(source_path):
     filename = "/".join(source_path.split("/")[-3:])
     path = basepath + filename
     return path
-#
-# if os.path.exists("X_train.npy"):
-#     X_train = np.load("X_train.npy")
-#     y_train = np.load("y_train.npy")
-# else:
-#     correction = .15
-#
-#     for line in lines:
-#         if line[0] == "center":
-#             continue
-#         image = cv2.imread(file_path(line[0]))
-#         images.append(image)
-#         steering_center = float(line[3])
-#         measurements.append(steering_center)
-#
-#         img_left = cv2.imread(file_path(line[1]))
-#         images.append(img_left)
-#         measurements.append(steering_center + correction) #left
-#
-#         images.append(np.fliplr(img_left))
-#         measurements.append(-(steering_center + correction)) #left
-#
-#         img_right = cv2.imread(file_path(line[2]))
-#         images.append(img_right)
-#         measurements.append(steering_center - correction) #right
-#
-#         images.append(np.fliplr(img_right))
-#         measurements.append( - (steering_center - correction))  # right
-#
-#         images.append(np.fliplr(image))
-#         measurements.append(-steering_center)
-#
-#     X_train = np.array(images)
-#     y_train = np.array(measurements)
-#     np.save("X_train.npy", X_train)
-#     np.save("y_train.npy", y_train)
 
 def generator(samples, batch_size=32):
     correction = .9
@@ -130,7 +94,6 @@ model = Sequential([
 adam = keras.optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.1)
 model.compile(loss='mse', optimizer=adam)
 
-# model.fit(X_train, y_train, epochs=5, validation_split=.2, shuffle=True)
 # fit model using generator, dataset may not fit in memory otherwise.
 model.fit_generator(train_generator, len(train_samples)/batch_size,
                     validation_data=validation_generator, validation_steps=len(validation_samples)/batch_size,
